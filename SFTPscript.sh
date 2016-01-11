a59ac3ec1cc5d840dc672d82f4f1a11c88a7bc2c
kowalsk <pkowalski@OSXpkowalski.local>
2016-01-11 10:09:23 +0000

    One file only transfer
    Complete change to transfer one file at a time (when passed as argument
    to the script). It is to be wrapped using another script that will run
    it more times if there are more files.

--- SFTPscript.sh
+++ SFTPscript.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-HOST='192.168.39.12'
 
-(
-  for file in ftpsource/*.txt; do
-    mv ${file} ${file}.tmp
-  done
-)
+#keeping original name just in case
+filename=$(basename $1 .tmp)
 
-sftp $HOST << EOT
+#renaming original file to .tmp to show it is being processed
+mv $1 $1.tmp
+ls
+
+#connecting to SFTP using keys - not passwords
+sftp 192.168.39.12 << EOT
 cd PawelTest
-mput ftpsource/*
+put $1.tmp
+ls
+rename $1.tmp $1
+ls
 bye
 EOT
-
-(
-  for file in *.tmp; do
-    echo mv ${file} ${file}
-  done
-)
